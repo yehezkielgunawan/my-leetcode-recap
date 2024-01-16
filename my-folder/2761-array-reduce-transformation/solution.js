2684fae0d6eb77dce776fbcc00449e2ff762ba82
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-01-16 04:08:20 +0000

    [LeetCode Sync] Runtime - 56 ms (43.40%), Memory - 42.4 MB (100.00%)

---
+++ my-folder/2761-array-reduce-transformation/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let res = init
+    if(nums.length < 1){
+        return res
+    }
+    for(let i=0; i<nums.length; i++){
+        res = fn(res, nums[i])
+    }
+    return res
+};
