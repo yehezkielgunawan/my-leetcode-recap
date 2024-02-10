d023d2331fd2223d7ae91f7395b844fc9c463006
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-02-10 07:49:58 +0000

    Sync LeetCode submission - Palindromic Substrings (javascript)

--- my-folder/problems/palindromic_substrings/solution.js
+++ my-folder/problems/palindromic_substrings/solution.js
@@ -3,21 +3,21 @@
  * @return {number}
  */
 var countSubstrings = function(s) {
-    let counter = 0
-   const isPalindrome = (s, left, right) => {
-       while(left < right){
-           if(s.charAt(left++) !== s.charAt(right--)) return false
+    let counterTotal = 0
+   const palindromeCount = (s, left, right) => {
+        let counter = 0
+       while(left>=0 && right<s.length && s.charAt(left--) === s.charAt(right++)){
+           counter++
        }
-       return true
+       return counter
    }
    
    for(let i=0; i<s.length; i++){
-       for(let j=i; j<s.length; j++){
-           if(isPalindrome(s, i, j)){
-               counter++
-           }
-       }
+       let even = palindromeCount(s, i, i+1)
+       let odd = palindromeCount(s, i-1, i+1)
+
+       counterTotal += even + odd + 1
    }
 
-    return counter;
+    return counterTotal;
 };
