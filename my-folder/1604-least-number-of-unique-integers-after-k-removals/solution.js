d1e25ac846a59448ea7efc5c83f480a3d31e5b37
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2024-02-16 03:35:34 +0000

    [LeetCode Sync] Runtime - 198 ms (28.17%), Memory - 84.5 MB (23.63%)

---
+++ my-folder/1604-least-number-of-unique-integers-after-k-removals/solution.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findLeastNumOfUniqueInts = function(arr, k) {
+    let tempMap = new Map()
+
+    for(let i=0; i<arr.length; i++){
+        if(!tempMap.has(arr[i])){
+            tempMap.set(arr[i], 1)
+        } else{
+            let tempVal = tempMap.get(arr[i])
+            tempMap.set(arr[i], ++tempVal)
+        }
+    }
+   const sortedMap = new Map([...tempMap.entries()].sort((a,b) => a[1]-b[1]))
+   let uniqueCount = sortedMap.size
+   
+   for(const [key, value] of sortedMap){
+       if(k >= value){
+           k-=value
+           uniqueCount--
+       } else{
+           break;
+       }
+   }
+   return uniqueCount
+};
